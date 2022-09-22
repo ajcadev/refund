10a52e5f1847119e8f037f1d99d333b3c50cf5ea
Cardoso <cardosoassuncao@gmail.com>
2022-09-22 13:47:26 -0400

    add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,48 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  '*, *::before, *::after': {
+    boxSizing: 'border-box',
+  },
+  
+  '*': {
+    margin: '0',
+  },
+  
+  'html, body, #__next': {
+    height: '100%',
+  },
+  
+  body: {
+    lineHeight: '1.5',
+    backgroundColor: '$base-background',
+    color: '$base-text',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+  
+  'img, picture, video, canvas, svg': {
+    display: 'block',
+    maxWidth: '100%',
+  },
+  
+  'input, button, textarea, select': {
+    font: 'inherit',
+  },
+  
+  'p, h1, h2, h3, h4, h5, h6': {
+    overflowWrap: 'break-word',
+  },
+  
+  '#__next': {
+    isolation: 'isolate',
+  },
+
+  'input::-webkit-outer-spin-button,input::-webkit-inner-spin-button': {
+    '-webkit-appearance': 'none',
+    margin: 0,
+  },
+
+  'input[type="number"]': {
+    '-webkit-appearance': 'textfield',
+  },
+})
