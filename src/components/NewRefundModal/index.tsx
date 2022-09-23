3a8da9e509043412ef4c4d3f4788d4cbb509b0c3
Cardoso <cardosoassuncao@gmail.com>
2022-09-23 09:21:50 -0400

    add form

--- src/components/NewRefundModal/index.tsx
+++ src/components/NewRefundModal/index.tsx
@@ -1,5 +1,8 @@
-import { Cross2Icon } from "@radix-ui/react-icons";
+import { CheckIcon, ChevronDownIcon, ChevronUpIcon, Cross2Icon } from "@radix-ui/react-icons";
+import { Button } from "../../styles/UI/Button";
 import { DialogClose, DialogContent, DialogDescription, DialogIconButton, DialogOverlay, DialogPortal, DialogTitle } from "../../styles/UI/Dialog";
+import { SelectContent, SelectIcon, SelectItem, SelectItemIndicator, SelectItemText, SelectPortal, SelectRoot, SelectScrollDownButton, SelectScrollUpButton, SelectTrigger, SelectValue, SelectViewport } from "../../styles/UI/Select";
+import { Fieldset, Flex, Input, Label } from "./styles";
 
 export function NewRefundModal(){
   return(
@@ -8,6 +11,79 @@ export function NewRefundModal(){
       <DialogContent>
         <DialogTitle>Novo reembolso</DialogTitle>
         <DialogDescription>Adicione um novo reembolso e aguarde nosso feedback.</DialogDescription>
+        <form action="">
+          <Fieldset>
+            <Label htmlFor="event">Evento</Label>
+            <SelectRoot>
+              <SelectTrigger id="event" aria-label="Event">
+                <SelectValue placeholder="Selecione um evento…" />
+                <SelectIcon>
+                  <ChevronDownIcon />
+                </SelectIcon>
+              </SelectTrigger>
+              <SelectPortal>
+                <SelectContent>
+                  <SelectScrollUpButton>
+                    <ChevronUpIcon />
+                  </SelectScrollUpButton>
+                  <SelectViewport>
+                    <SelectItem value="1828">
+                      <SelectItemText>Creche</SelectItemText>
+                      <SelectItemIndicator>
+                        <CheckIcon />
+                      </SelectItemIndicator>
+                    </SelectItem>
+                    <SelectItem value="1827">
+                      <SelectItemText>Médico</SelectItemText>
+                      <SelectItemIndicator>
+                        <CheckIcon />
+                      </SelectItemIndicator>
+                    </SelectItem>
+                    <SelectItem value="1966">
+                      <SelectItemText>Odonto</SelectItemText>
+                      <SelectItemIndicator>
+                        <CheckIcon />
+                      </SelectItemIndicator>
+                    </SelectItem>
+                  </SelectViewport>
+                  <SelectScrollDownButton>
+                    <ChevronDownIcon />
+                  </SelectScrollDownButton>
+                </SelectContent>
+              </SelectPortal>
+            </SelectRoot>
+          </Fieldset>
+          <Fieldset>
+            <Label htmlFor="numberDoc">Número</Label>
+            <Input
+              id="numberDoc"
+              type="text"
+              placeholder='Número do documento'
+              required
+            />
+          </Fieldset>
+          <Fieldset>
+            <Label htmlFor="issueDate">Emissão</Label>
+            <Input
+              id="issueDate"
+              type="date"
+              placeholder='Emissão'
+              required
+            />
+          </Fieldset>
+          <Fieldset>
+            <Label htmlFor="value">Valor</Label>
+            <Input
+              id="value"
+              type="number"
+              placeholder='Valor'
+              required
+            />
+          </Fieldset>
+          <Flex css={{ marginTop: 25, justifyContent: 'flex-end' }}>
+            <Button type="submit" variant="green">Salvar</Button>
+          </Flex>
+        </form>
         <DialogClose asChild>
           <DialogIconButton aria-label="Close">
             <Cross2Icon />
---
+++ src/components/NewRefundModal/styles.ts
@@ -0,0 +1,40 @@
+import { styled } from "../../styles";
+
+export const Flex = styled('div', { display: 'flex' });
+
+export const Fieldset = styled('fieldset', {
+  all: 'unset',
+  display: 'flex',
+  gap: 20,
+  alignItems: 'center',
+  marginBottom: 15,
+});
+
+export const Label = styled('label', {
+  fontSize: 15,
+  color: '$violet11',
+  width: 90,
+  textAlign: 'right',
+});
+
+export const Input = styled('input', {
+  all: 'unset',
+  width: '100%',
+  flex: '1',
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: 4,
+  padding: '0 10px',
+  fontSize: 15,
+  lineHeight: 1,
+  color: '$violet11',
+  $$shadow: '$colors$violet7',
+  boxShadow: '0 0 0 1px $$shadow',
+  height: 35,
+
+  '&:focus': {
+    $$shadow: '$colors$violet8', 
+    boxShadow: '0 0 0 2px $$shadow'
+  },
+});
